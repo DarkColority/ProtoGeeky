6e99a48f0b1052704c1b4dac847d35670955270e
Juan Pablo Toro Monsalve <juantorom.97@gmail.com>
2019-07-21 16:42:32 -0500

    Add repository

---
+++ Geeky.Web/Data/Repository.cs
@@ -0,0 +1,57 @@
+﻿using Geeky.Web.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Geeky.Web.Data
+{
+
+
+public class Repository
+    {
+        private readonly DataContext context;
+
+        public Repository(DataContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<Evento> GetEventos()
+        {
+            return this.context.Eventos.OrderBy(p => p.Nombre);
+        }
+
+        public Evento GetEvento(int id)
+        {
+            return this.context.Eventos.Find(id);
+        }
+
+        public void AddEvento(Evento Evento)
+        {
+            this.context.Eventos.Add(Evento);
+        }
+
+        public void UpdateEvento(Evento Evento)
+        {
+            this.context.Update(Evento);
+        }
+
+        public void RemoveEvento(Evento Evento)
+        {
+            this.context.Eventos.Remove(Evento);
+        }
+
+        public async Task<bool> SaveAllAsync()
+        {
+            return await this.context.SaveChangesAsync() > 0;
+        }
+
+        public bool EventoExists(int id)
+        {
+            return this.context.Eventos.Any(p => p.Id == id);
+        }
+    }
+
+}
+
