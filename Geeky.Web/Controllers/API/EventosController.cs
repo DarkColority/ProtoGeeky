17dc9b233a31e924ad79929ce8e27937078bec18
Juan Pablo Toro Monsalve <juantorom.97@gmail.com>
2019-07-27 12:49:21 -0500

    Add API Events

---
+++ Geeky.Web/Controllers/API/EventosController.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace Geeky.Web.Controllers.API
+{
+    using Geeky.Web.Data;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[Controller]")]
+    public class EventosController : Controller
+    {
+        private readonly IEventRepository eventRepository;
+
+        public EventosController(IEventRepository eventRepository)
+        {
+            this.eventRepository = eventRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetEvents()
+        {
+            return Ok(this.eventRepository.GetAll());
+        }
+    }
+}
